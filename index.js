6c63db7cb7758ceda4582fde7c376820a6751fbf
Dane Schneider <dane.schneider@gmail.com>
2017-07-18 03:10:50 -0400

    Add define param for merging in additional vars

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ function EnvkeyWebpackPlugin(opts) {
     throw new Error("'permitted' key required to specifiy vars whitelisted for client.")
   }
 
+  if (opts.define){
+    for (k in opts.define){
+      defineParams[k] = JSON.stringify(opts.define[k])
+    }
+  }
+
   if (opts.permitted.indexOf("NODE_ENV") > -1){
     defineParams.NODE_ENV = JSON.stringify(process.env.NODE_ENV)
   }
