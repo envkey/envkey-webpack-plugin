7e244f077aa55ea3138ac7bcb4d6d0d7a1736e7b
Dane Schneider <dane.schneider@gmail.com>
2018-05-21 16:01:42 -0700

    fix overrides - closes #2

--- index.js
+++ index.js
@@ -4,24 +4,21 @@ var webpack = require('webpack'),
 module.exports = EnvkeyWebpackPlugin
 
 function EnvkeyWebpackPlugin(opts) {
-  var env = envkey.load(opts),
-      defineParams = {}
+  envkey.load(opts)
+
+  var defineParams = {}
 
   if (!opts.permitted || !opts.permitted.length){
     throw new Error("'permitted' key required to specifiy vars whitelisted for client.")
   }
+
+  for (k of opts.permitted) defineParams[k] = JSON.stringify(process.env[k])
 
   if (opts.define){
     for (k in opts.define){
       defineParams[k] = JSON.stringify(opts.define[k])
     }
   }
-
-  if (opts.permitted.indexOf("NODE_ENV") > -1){
-    defineParams.NODE_ENV = JSON.stringify(process.env.NODE_ENV)
-  }
-
-  for (k in env) defineParams[k] = JSON.stringify(env[k])
 
   return new webpack.DefinePlugin({"process.env": defineParams})
 }
